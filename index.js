f191f2f1c4e1a8c563e969469374b2865beb3ea7
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-10-09 02:14:40 +0800

    afvfd

--- index.js
+++ index.js
@@ -21,3 +21,38 @@ export const useLogger = (value) => {
     }
   }, [value])
 }
+
+export const useStepForm = (steps = []) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+  const next = () => {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i
+      return i + 1
+    })
+    // if(currentStepIndex===steps.length-1) return
+  }
+  const back = () => {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i
+      return i - 1
+    })
+  }
+
+  const goTo = (index) => {
+    setCurrentStepIndex(index)
+  }
+
+  return {
+    currentStepIndex,
+    steps,
+    step: steps[currentStepIndex],
+    goTo,
+    next,
+    pagination: {
+      next: currentStepIndex < steps.length - 1,
+      prev: currentStepIndex > 0,
+    },
+    back,
+  }
+}
